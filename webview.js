409295e7a3669d5558e46a178f631a9890999be5
Peter Mein <peter@infratron.io>
2019-10-01 09:34:17 +0200

    :feat: added logo's and badges

--- webview.js
+++ webview.js
@@ -31,12 +31,12 @@ window.addEventListener('beforeunload', async () => {
 
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
-    const elements = document.querySelectorAll('.CxUIE, .unread, ._0LqQ');
+    const elements = document.querySelectorAll('.app ul.menu > li');
     let count = 0;
 
     for (let i = 0; i < elements.length; i += 1) {
-      if (elements[i].querySelectorAll('.P6z4j').length === 1 && elements[i].querySelectorAll('*[data-icon="muted"]').length === 0) {
-        count += 1;
+      if (elements[i].querySelectorAll('a span.count').length === 1) {
+        count += parseInt(elements[i].querySelectorAll('a span.count')[0].innerHTML);
       }
     }
 
